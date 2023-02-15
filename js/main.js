4eb0b3248cc2a60669d917d87ac5efb716b42a98
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-15 17:08:22 +0100

    contatti dinamici

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 // Milestone 1
 // ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
-// dall’interlocutore (bianco) assegnando due classi CSS diverse
+// dall’interlocutore (bianco) assegnando due classi CSS diverse   OK!
+
 // ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
-// nome e immagine di ogni contatto
+// nome e immagine di ogni contatto  OK!
+
 // Milestone 2
 // ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
 // messaggi relativi al contatto attivo all’interno del pannello della conversazione
+
 // ● Click sul contatto mostra la conversazione del contatto cliccato
 // Milestone 3
+
 // ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
 // “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+
 // ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
 // un “ok” come risposta, che apparirà dopo 1 secondo.
+
 // Milestone 4
 // ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
 // contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
@@ -28,7 +34,7 @@ const settVue = Vue.createApp({
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: './img/avatar_1.jpg',
+                    avatar: '_1',
                     visible: true,
                     messages: [
                         {
@@ -191,4 +197,4 @@ const settVue = Vue.createApp({
         }
     }
 })
-settVue.mount("#root")
+settVue.mount("#app")
